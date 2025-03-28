3ac7688e75af9b155045eb6bcc4729d358e54077
Crystiana <crysmarg24@gmail.com>
2025-03-27 21:37:55 -0400

    first commit

---
+++ src/modules/home/ui/auth/ui/components/auth-button.tsx
@@ -0,0 +1,11 @@
+import { UserCircleIcon } from "lucide-react";
+export const AuthButton = () => {
+    // TODO: Add different auth states
+  return (
+   <button
+   className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20 rounded-full shadow-none">
+    <UserCircleIcon />
+    Sign in
+   </button>
+  );
+}
